5830112e3238ef2ddc61aed5258d9c56712143c1
leicester70 <lestercheong70@gmail.com>
2023-12-30 04:20:41 +0800

    bugfix for build

--- src/context/store.ts
+++ src/context/store.ts
@@ -11,7 +11,7 @@ export const store = configureStore({
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 type AppDispatch = typeof store.dispatch;
 
